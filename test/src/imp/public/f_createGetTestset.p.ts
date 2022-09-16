0d8d6e422e30ce6ead79a2990a05ed1dde5c24e9
corno <corno.astn.a@schraverus.com>
2022-09-16 14:52:21 +0200

    .

--- test/src/imp/public/f_createGetTestset.p.ts
+++ test/src/imp/public/f_createGetTestset.p.ts
@@ -10,7 +10,7 @@ import * as api from "../../interface"
 
 import * as pub from "../../../../pub"
 
-export const createGetTestset: api.FCreateGetTestset = ($d) => {
+export const f_createGetTestset: api.FCreateGetTestset = ($d) => {
     return () => {
 
         const builder = pm.createDictionaryBuilder<test.TTestElement>(
